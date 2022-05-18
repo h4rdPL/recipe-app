cc94541540414104f6dc079fa747b2c97740f77e
Matthew <76441552+h4rdPL@users.noreply.github.com>
2022-05-18 22:44:50 +0200

    update database name

--- src/libs/db_connect.php
+++ src/libs/db_connect.php
@@ -3,7 +3,7 @@
     $host = 'localhost';
     $user = 'root';
     $password = '';
-    $db_name = 'database_project';
+    $db_name = 'napkin_foods';
     $conn = mysqli_connect($host,$user,$password,$db_name);
 
 
